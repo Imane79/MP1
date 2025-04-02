651b980c6a16afa1269310c2da2a7d5b159695ef
Imane <imane.tech@outlook.com>
2025-04-02 21:29:07 +0300

    first commit

---
+++ project1.py
@@ -0,0 +1,7 @@
+import tkinter as tk  # tkinter is Python’s built-in GUI library.
+from tkinter import ttk, filedialog, messagebox
+# ttk gives us modern widgets like Combobox, filedialog allows file selection (e.g., for Excel import)
+# messagebox will be used for warnings/errors/success.
+import pandas as pd  # pandas is for reading Excel and exporting .xls.
+import random  # random is for the "Randomize" feature.
+import json  # is for exporting to .json format.
